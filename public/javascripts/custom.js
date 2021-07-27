a943201eae7b9ed833a40b9236d8961de2f98846
basildybala <akbasil123@gmail.com>
2021-07-27 17:17:49 +0530

    Malayalam Half adeed

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -3,11 +3,7 @@
        const myLazyLoad= new LazyLoad ({
            elements_selector:".lazy-photo"
        });
-   
-
-
-
-//Read More popup
+  //Read More popup
   const btnn =document.querySelector('.readmore-btn');
    const storyContainer=document.querySelector('.story-readmore');
    const closee =document.querySelector('.closee');
@@ -20,5 +16,28 @@
     storyContainer.classList.remove('show')
    })
 
-
+   //Trailor Btn
+   const btn =document.querySelector('.trailor-btn');
+   const videoContainer=document.querySelector('.video-container');
+   const close =document.querySelector('.close');
+   
+   
+   btn.addEventListener('click',()=>{
+       videoContainer.classList.add('show')
+   })
+   close.addEventListener('click',()=>{
+       videoContainer.classList.remove('show')
+   })
+   //Read More English 
+   const btnenglish =document.querySelector('.readmore-btn-english');
+   const storyContainerenglish=document.querySelector('.story-readmore-english');
+   const closenglish =document.querySelector('.closee-english');
+   
+   
+   btnenglish.addEventListener('click',()=>{
+    storyContainerenglish.classList.add('show')
+   })
+   closenglish.addEventListener('click',()=>{
+    storyContainerenglish.classList.remove('show')
+   })
  
